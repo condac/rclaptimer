407399bb40307ea0691b3727def5ecb153f2f74c
condac <gitlab@burns.nu>
2013-06-04 18:51:21 +0200

    filename selection code

--- laptimer/laptimer.ino
+++ laptimer/laptimer.ino
@@ -25,6 +25,24 @@ void setup()
   
   lcd.begin(16, 2);
   lcd.print(".");
+  
+  //filename number
+  /*
+  // create a new file
+  char filename[] = "LOGGER00.CSV";
+  for (uint8_t i = 0; i < 100; i++) {
+    filename[6] = i/10 + '0';
+    filename[7] = i%10 + '0';
+    if (! SD.exists(filename)) {
+      // only open a new file if it doesn't exist
+      logfile = SD.open(filename, FILE_WRITE); 
+      break;  // leave the loop!
+    }
+  }
+  
+  if (! logfile) {
+    error("couldnt create file");
+  }*/
 }
 
 void loop()
