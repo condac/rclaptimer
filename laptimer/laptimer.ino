1e8cc7bfd3234dbcb57fc2b0320d223cae1401e4
condac <gitlab@burns.nu>
2013-06-11 14:38:49 +0200

    testing sd write

--- laptimer/laptimer.ino
+++ laptimer/laptimer.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
+#include <SD.h>
 
 const int buttonPin = A0; 
 const int buzzerPin = 13;
@@ -7,7 +8,7 @@ int test;
 int laps = 0;
 int buzzerLength = 500;
 int mintime = 2000;
-LiquidCrystal lcd( 8, 9, 4, 5, 6, 7 );
+LiquidCrystal lcd( 8, 9, 3, 5, 6, 7 ); //4 och 3 byt plats pga sdkort
 String extid = "12.34";
 
 unsigned long best = 9999999;
@@ -18,11 +19,12 @@ unsigned long time;
 unsigned long last1;
 float diff;
 float timefloat;
-
+File logfile;
+char filename[] = "LOGGER00.CSV";
 
 void setup()
 {
-    
+    Serial.begin(9600);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);  
   pinMode(buzzerPin, OUTPUT);
@@ -30,23 +32,40 @@ void setup()
   lcd.begin(16, 2);
   lcd.print(".");
   
+  Serial.print("Initializing SD card...");
+  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
+  // Note that even if it's not used as the CS pin, the hardware SS pin 
+  // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
+  // or the SD library functions will not work. 
+   pinMode(10, OUTPUT);
+   
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+  
   //filename number
-  /*
+  
   // create a new file
-  char filename[] = "LOGGER00.CSV";
+  
   for (uint8_t i = 0; i < 100; i++) {
     filename[6] = i/10 + '0';
     filename[7] = i%10 + '0';
     if (! SD.exists(filename)) {
       // only open a new file if it doesn't exist
       logfile = SD.open(filename, FILE_WRITE); 
+      logfile.close();
+      Serial.print(filename);
+      Serial.println("logfile created");
       break;  // leave the loop!
     }
   }
   
   if (! logfile) {
-    error("couldnt create file");
-  }*/
+    lcd.print("SD ERROR");
+    delay(5000);
+  }
 }
 
 void loop()
@@ -61,6 +80,13 @@ void loop()
       
       //diff
       diff=((float)time-(float)last1) /1000.0;
+      
+      //Write to SD
+      logfile = SD.open(filename, FILE_WRITE);
+      logfile.println(time);
+      logfile.close();
+      Serial.print(time);
+      Serial.println(" Written to SD");
       
       if (time < best) {
         best = time;
